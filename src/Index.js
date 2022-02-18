c50d528159affddcf989265dcb501f43d5cff68d
arsan13 <arsan.anbu97@gmail.com>
2022-02-18 10:05:57 +0530

    Added Loader for async storage

--- src/Index.js
+++ src/Index.js
@@ -1,22 +1,40 @@
 import React, {useEffect, useState} from 'react';
+import {View} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {getData, storeData, removeData} from './utils/LocalStorage';
+import Loading from './components/Loading';
 import AuthStack from './navigation/AuthStack';
 import AppStack from './navigation/AppStack';
 
 const Index = () => {
   const [token, setToken] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   const handleToken = value => {
+    setLoading(true);
     if (value === null) removeData();
     else storeData(value);
     setToken(value);
+    setLoading(false);
   };
 
   useEffect(() => {
     // Check async storage if user is logged in
-    getData().then(val => setToken(val));
+    (async function () {
+      setLoading(true);
+      const val = await getData();
+      setToken(val);
+      setLoading(false);
+    })();
   }, []);
+
+  if (loading) {
+    return (
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <Loading />
+      </View>
+    );
+  }
 
   return (
     <NavigationContainer>
